ae2ccc9c4a5cea813eb31fe5b99b3b87e8bf38c2
nuralivafoyev <nurikboy555@gmail.com>
2024-03-02 22:41:28 +0500

    login

---
+++ assets/js/checkbox.js
@@ -0,0 +1,8 @@
+var unchek = document.getElementById("outer_bx-pg");
+var check = document.getElementById("outer_bx");
+
+check.addEventListener('click', function(){
+    unchek.checked = false;
+})
+
+// unchek.checked = true;
